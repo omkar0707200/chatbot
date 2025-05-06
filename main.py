e7af83d5fa869026b388716e8f894462ad76e17b
omkar0707200 <kalbhoromkar8@gamil.com>
2025-05-06 13:01:47 +0530

    Added new feature and fixed bugs

--- main.py
+++ main.py
@@ -16,8 +16,15 @@ def home():
 def chat():
     data = request.get_json()
     query = data.get("query")
+    if( data.get("old_query") != None and (query=='yes' or query=='yes please' or query=='okay' or query=='ask ai' or query=='go ahead')):
+        askai = data.get("old_query")
+    else:
+        askai = 'no'
+
     if not query:
         return jsonify({"error": "Missing 'query' in request"}), 400
-
-    response = handle_user_query(query)
+    if(askai == 'no'):
+        response = handle_user_query(query)
+    else:
+        response = get_fallback_response(askai)
     return jsonify({"response": response})
